e808e663db3a8c095f73418c36ca161b5403bceb
unknown <quentin.bernet@bluewin.ch>
2020-05-23 14:39:59 +0200

    First draft of explainWithRule

--- rule_generation.py
+++ rule_generation.py
@@ -11,9 +11,21 @@ class RuleGenerator():
       list_rules = []
       dict_ = arbre.enfants
       for key, child in dict_.items():
-        string = '{}'.format(arbre.attribut) + " = " + key
+        string = [arbre.attribut, key]
         list_rules = list_rules + self.rec(child, partial_rule + [string])
     return list_rules
 
   def __init__(self, arbre):
     self.rules = self.rec(arbre, [])
+
+def explainWithRule(rules, example):
+  attibut = rules[0][0][0][0]
+  valAtAttribut = example[1][attibut]
+  def filter_(rule):
+    inspected = rule[0][0][1]
+    return inspected == valAtAttribut
+  filtered = filter(filter_, rules)
+  removedAttribut = map(lambda rule: [rule[0][1:],rule[1]], filtered)
+
+
+  return list(removedAttribut)
