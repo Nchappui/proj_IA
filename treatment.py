5d7d5d14a4e1f1557ac50bf89598b20b3b89f8e9
Nolan Chappuis <nolan.chappuis@epfl.ch>
2020-05-23 14:41:16 +0200

    Continue treatment

--- treatment.py
+++ treatment.py
@@ -7,13 +7,12 @@ class Treatment():
         for donnee in donnees:
             changes=0
             while changes <=num_treats:
-                if func(donnee, rules) == 0 and changes == 0:
-                    #patient is not sick
-                    break
-                elif func(donnee, rules) == 1:
-                    changedonnee(donnee)
-                    
-                changes+=1
+                if self.func(donnee, rules) == 1:
+                    self.changedonnee(donnee, changes)
+
+                else:
+                    #patient is not sick anymore
+                    result +=1
 
         return 0
 
@@ -21,3 +20,11 @@ class Treatment():
     
     def __init__(self, donnees, rules):
         self.treat = self.treatment(donnees,0, rules)
+
+    def func(self, donnee, rules):
+        #just to remove errors
+        return 1
+
+    def changedonnee(self, donnee, changes):
+        #changes a donne and updates the number of changes made
+        return 1
